a41c326229096ed06124e05b396d07012ec4aade
Charlie Pham <phamc5@wit.edu>
2020-03-28 18:04:12 -0400

    Fix FizzBuzz

--- src/main/java/com/wkrzywiec/medium/noticeboard/service/FizzBuzz.java
+++ src/main/java/com/wkrzywiec/medium/noticeboard/service/FizzBuzz.java
@@ -18,33 +18,32 @@ import static com.wkrzywiec.medium.noticeboard.mapper.AuthorMapper.INSTANCE;
 public class FizzBuzz {
 	String processNumber(int num)
 	{
-        return null;
-		// if(num%15 == 0 )
-		// {
-		// 	return "FizzBuzz";
-		// }
-		// if(num%3 == 0 )
-		// {
-		// 	return "Fizz";
-		// }
-		// if (num%5 == 0)
-		// {
-		// 	return "Buzz";	
-		// }
-		// return String.valueOf(num);
+		if(num%15 == 0 )
+		{
+			return "FizzBuzz";
+		}
+		if(num%3 == 0 )
+		{
+			return "Fizz";
+		}
+		if (num%5 == 0)
+		{
+			return "Buzz";	
+		}
+		return String.valueOf(num);
 	}
 	
 	String processNumbers (int[] numList)
 	{
 		String list = "";
-		// for (int i = 0; i < numList.length; i++)
-		// {
-		// 	list += processNumber(numList[i]);
-		// 	if (i < numList.length-1)
-		// 	{
-		// 		list += ", ";
-		// 	}
-		// }
+		for (int i = 0; i < numList.length; i++)
+		{
+			list += processNumber(numList[i]);
+			if (i < numList.length-1)
+			{
+				list += ", ";
+			}
+		}
 		return list;
 	}
 
